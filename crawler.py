77d90767b3fbab2e4944032cd1174d4b0d1a1a34
Bharath <bharathraja@live.com>
2013-01-18 06:00:11 +0530

    added and tested beautifull soup

--- crawler.py
+++ crawler.py
@@ -2,11 +2,23 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import os
 
+def replace_all(text, dic):
+    for i, j in dic.items():
+        text = text.replace(i, j)
+    return text
+
 u=urlopen('http://www.mtcbus.org/Places.asp')
 data=str(u.read())
-soup = BeautifulSoup(data)
-soup=soup.prettify()
+#print(data[0:30].replace('\\n','\n'))
+data=replace_all(data,{'\\\'':'\'','\\\"':'\"','\\n':'\n','\\r':'','\\t':'	'});
+
+print (str(soup.title))
 
 f=open('data/list.html','w')
 f.write(soup)
+f.close()
+
+f=open('data/list.html','r')
+soup = BeautifulSoup(f.read())
+soup=soup.prettify()
 f.close()
