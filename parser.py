dc76a0c5901ecae6c50afedca1a22c9176bae351
Bharath <bharathraja@live.com>
2013-01-18 09:38:29 +0530

    parser edit

--- parser.py
+++ parser.py
@@ -1,20 +1,20 @@
 from bs4 import BeautifulSoup
 
-# f=open('data/stopList.html','r')
-# soup = BeautifulSoup(f.read())
-# f.close()
-# open('data/stopList.html','w').close()
-# f=open('data/stopList.html','w')
-# f.write(str(soup))
-# f.close()
+f=open('data/stopList.html','r')
+soup = BeautifulSoup(f.read())
+f.close()
+open('data/stopList.html','w').close()
+f=open('data/stopList.html','w')
+f.write(str(soup))
+f.close()
 
-# myList=[]
-# temp=soup.find('select').find_all('option')
-# for op in temp:
-# 	myList.append(str(BeautifulSoup(str(op)).string))
-# myList.sort()
+myList=[]
+temp=soup.find('select').find_all('option')
+for op in temp:
+	myList.append(str(BeautifulSoup(str(op)).string))
+myList.sort()
 
-# f=open('data/list.html','w')
-# for name in myList:
-# 	f.write(name+'\n')
-# f.close()
+f=open('data/list','w')
+for name in myList:
+	f.write(name+'\n')
+f.close()
