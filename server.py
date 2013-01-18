1f8c2340561d787cb3c7bbdf0f6c68ef31517a32
Bharath <bharathraja@live.com>
2013-01-18 14:40:30 +0530

    busSearch ui done

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from jinja2 import Environment, PackageLoader
 
+app = Flask(__name__)
 env = Environment(loader=PackageLoader('yourapplication', 'templates'))
 
 @app.route("/")
 def hello():
 	template = env.get_template('busSearch.html')
-	print template.render(localStopList=[])
-    return template.render(localStopList=[])
+	print template.render(localStopList='[]')
+	return template.render(localStopList='[]')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
